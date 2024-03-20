1599527d7f23716e7980c6b33590b44e3b341bde
aaron <lixin53517@gmail.com>
2024-03-20 11:37:11 +0800

    forge fmt

--- script/Vault.s.sol
+++ script/Vault.s.sol
@@ -5,18 +5,13 @@ import "forge-std/Script.sol";
 
 import "../src/Vault.sol";
 
-
-
 contract VaultScript is Script {
-    function setUp() public {
-
-    }
+    function setUp() public {}
 
     function run() public {
         string memory mnemonic = "test test test test test test test test test test test junk";
-        (address deployer, ) = deriveRememberKey(mnemonic, 0);
+        (address deployer,) = deriveRememberKey(mnemonic, 0);
         vm.startBroadcast(deployer);
-
 
         VaultLogic logic = new VaultLogic(bytes32("0x1234"));
         Vault vault = new Vault(address(logic));
@@ -24,8 +19,5 @@ contract VaultScript is Script {
 
         vault.deposite{value: 0.1 ether}();
         vm.stopBroadcast();
-
-
-
     }
 }
